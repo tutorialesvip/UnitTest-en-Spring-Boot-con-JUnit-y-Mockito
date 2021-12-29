f61436d8a1d5200542dd2d03d7832c99d8bb9849
javiersm79 <jjlion79@gmail.com>
2021-12-29 18:38:08 -0300

    HotFix Class CountryResponse (Getters faltantes)
    HotFix para responder el body completo del la clase CountryResponse y no solo el nombre del país

--- src/main/java/com/tutorialesvip/tutorialunittest/models/CountryResponse.java
+++ src/main/java/com/tutorialesvip/tutorialunittest/models/CountryResponse.java
@@ -16,22 +16,40 @@ public class CountryResponse {
         this.countryName = countryName;
     }
 
+    public String getCapitalName() {
+        return capitalName;
+    }
 
     public void setCapitalName(String capitalName) {
         this.capitalName = capitalName;
     }
 
+    public String getIndependenceDate() {
+        return independenceDate;
+    }
 
     public void setIndependenceDate(String independenceDate) {
         this.independenceDate = independenceDate;
+    }
+
+    public int getYearsOfIndependency() {
+        return yearsOfIndependency;
     }
 
     public void setYearsOfIndependency(int yearsOfIndependency) {
         this.yearsOfIndependency = yearsOfIndependency;
     }
 
+    public int getMonthsOfIndependency() {
+        return monthsOfIndependency;
+    }
+
     public void setMonthsOfIndependency(int monthsOfIndependency) {
         this.monthsOfIndependency = monthsOfIndependency;
+    }
+
+    public int getDayssOfIndependency() {
+        return dayssOfIndependency;
     }
 
     public void setDayssOfIndependency(int dayssOfIndependency) {
